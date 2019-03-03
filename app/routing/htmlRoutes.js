0055da457fbfcba158434059132001d9da067511
HaydnNeese <haydnneese@yahoo.com>
2019-03-03 16:32:37 -0600

    fixed home route

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,6 +3,13 @@ var path = require("path");
 
 
 module.exports = function (app) {
+    
+    //a default catch-all route that will return the user to the homepage
+    app.get("/", function (req, res) {
+        //map to "home.html"
+        res.sendFile(path.join(__dirname, "../public/home.html"))
+    })
+}
 
     //get route to display the survey page
     app.get("/survey", function (req, res) {
